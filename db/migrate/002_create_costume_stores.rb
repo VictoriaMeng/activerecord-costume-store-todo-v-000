e894652d4e597334fa1c57157e3dd342807aa53d
Victoria Meng <nights1ststar@protonmail.com>
2018-05-12 23:45:28 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -2,6 +2,13 @@
 class CreateCostumeStores < ActiveRecord::Migration[4.2]
   def change
     create_table :costume_stores do |c|
+      c.string :name,
+      c.string :location,
+      c.integer :costume_inventory,
+      c.integer :num_of_employees,
+      c.boolean :still_in_business,
+      c.string :opening_time,
+      c.string :closing_time
     end
   end
 end
