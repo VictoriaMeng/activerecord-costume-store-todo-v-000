06ca7ddbe20d70e60991bbce3dcff604ac3834df
Victoria Meng <nights1ststar@protonmail.com>
2018-05-12 23:58:33 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -2,6 +2,13 @@
 class CreateHauntedHouses < ActiveRecord::Migration[4.2]
   def change
     create_table :haunted_houses do |h|
+      h.string :name
+      h.string :location
+      h.string :theme
+      h.float :price
+      h.boolean :family_friendly
+      h.datetime :opening_date
+      h.datetime :closing_date
     end
   end
 end
